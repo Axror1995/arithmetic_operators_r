2c66ddbc37ffaf584e760cb9ed20f31749de6309
aror raximmov <axrorraximov108@gmail.com>
2022-10-23 00:40:10 +0500

    009

--- bin/arithmetic009.dart
+++ bin/arithmetic009.dart
@@ -2,10 +2,10 @@
 // the task condition is in the Readme file
 import 'dart:math';
 
-num func(int a, int b, int v, int c) {
-  return pow(a, b) + pow(v, c);
+num func(int a, int b,) {
+  return pow(a,2) + pow(b,2);
 }
 
 void main() {
-  print(func(3, 5, 6, 2));
+  print(func(1,5));
 }
