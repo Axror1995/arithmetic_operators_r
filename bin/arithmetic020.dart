bc852034e11e54333436656b05040f3efe5fcb7d
aror raximmov <axrorraximov108@gmail.com>
2022-10-22 20:57:38 +0500

    020

--- bin/arithmetic020.dart
+++ bin/arithmetic020.dart
@@ -1,4 +1,12 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+import 'dart:math';
+
+double func(int a, int b, int c) {
+  return (a +b)/ (2*c) ;
+}
+
+void main() {
+  print(func(2,4,6));
+}
